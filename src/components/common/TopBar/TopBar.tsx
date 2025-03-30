e419c8096cccc499c3029f3b743e1a2ccb44af72
Cecilia Sanare <ceci@sanare.dev>
2025-03-30 00:28:20 -0500

    feat(TopBar): more visual tweaks to fit the rest of the app

--- src/components/common/TopBar/TopBar.tsx
+++ src/components/common/TopBar/TopBar.tsx
@@ -4,25 +4,24 @@ import { useSelector } from 'react-redux';
 import { Settings } from 'lucide-react';
 import { Button } from '../Button';
 import { Link } from 'react-router-dom';
+import { TuiIcon } from '../TuiIcon';
 
 export const TopBar: FC = () => {
   const header = useSelector(selectHeader);
 
-  const image = header.image && <img className="hidden lg:inline-flex" src={header.image} />;
-
   return (
-    <div className="flex items-center px-10 bg-tui-dark min-h-[72px]">
-      <div className="text-2xl font-bold flex-1">Tyria UI</div>
-      <div className="flex items-center gap-5">
-        {image}
-        <div className="font-bold text-xl">{header.label}</div>
-        {image}
-      </div>
-      <div className="flex flex-1 gap-2 justify-end">
-        <Button as={Link} color="light" to="/settings">
-          <Settings />
-          <span className="hidden lg:inline-flex">Settings</span>
-        </Button>
+    <div className="flex px-6 bg-tui-dark min-h-[72px]">
+      <div className="flex flex-1 items-center justify-between mx-auto w-full max-w-[1200px]">
+        <div className="flex gap-4 items-center text-2xl font-bold">
+          {header.image && <TuiIcon icon={header.image} size={48} />}
+          Tyria UI - {header.label}
+        </div>
+        <div className="flex gap-2">
+          <Button as={Link} color="light" to="/settings">
+            <Settings />
+            <span className="hidden lg:inline-flex">Settings</span>
+          </Button>
+        </div>
       </div>
     </div>
   );
