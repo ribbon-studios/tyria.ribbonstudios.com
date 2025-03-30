f7b1ac60e24b41f0667b4118af41c9dbc53c93f2
Cecilia Sanare <ceci@sanare.dev>
2025-03-30 00:21:40 -0500

    fix(TopBar): improve responsiveness

--- src/components/common/TopBar/TopBar.tsx
+++ src/components/common/TopBar/TopBar.tsx
@@ -8,18 +8,20 @@ import { Link } from 'react-router-dom';
 export const TopBar: FC = () => {
   const header = useSelector(selectHeader);
 
+  const image = header.image && <img className="hidden lg:inline-flex" src={header.image} />;
+
   return (
     <div className="flex items-center px-10 bg-tui-dark min-h-[72px]">
       <div className="text-2xl font-bold flex-1">Tyria UI</div>
       <div className="flex items-center gap-5">
-        {header.image && <img src={header.image} />}
+        {image}
         <div className="font-bold text-xl">{header.label}</div>
-        {header.image && <img src={header.image} />}
+        {image}
       </div>
       <div className="flex flex-1 gap-2 justify-end">
         <Button as={Link} color="light" to="/settings">
           <Settings />
-          Settings
+          <span className="hidden lg:inline-flex">Settings</span>
         </Button>
       </div>
     </div>
