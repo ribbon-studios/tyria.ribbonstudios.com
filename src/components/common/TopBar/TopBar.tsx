3e1cdc57d28bcf32ab71c3105aa7e7e2863fb26b
Cecilia Sanare <ceci@sanare.dev>
2025-04-07 20:26:48 -0500

    chore: improve mobile responsiveness of the header

--- src/components/common/TopBar/TopBar.tsx
+++ src/components/common/TopBar/TopBar.tsx
@@ -1,40 +1,18 @@
-import { selectHeader } from '@/store/app.slice';
-import { Fragment, type FC } from 'react';
-import { useSelector } from 'react-redux';
+import { type FC } from 'react';
 import { Menu, Settings } from 'lucide-react';
 import { Button } from '../Button';
 import { NavLink } from 'react-router-dom';
-import { TuiIcon } from '../TuiIcon';
-import { TuiLink } from '../TuiLink';
+import { TuiHeader } from './TuiHeader';
 
 export const TopBar: FC<TopBar.Props> = ({ onSideBarToggle }) => {
-  const header = useSelector(selectHeader);
-
   return (
-    <div className="flex px-6 bg-tui-dark min-h-[72px]">
-      <div className="flex flex-1 items-center justify-between mx-auto w-full max-w-[1200px]">
-        <div className="flex gap-4 items-center text-lg md:text-2xl font-bold">
-          <Button className="inline-flex md:hidden" onClick={() => onSideBarToggle()}>
-            <Menu />
-          </Button>
-          <TuiIcon icon={header.image ?? '/favicon.png'} size={48} />
-          {header.breadcrumbs.length > 0 ? (
-            <>
-              <TuiLink className="hidden lg:inline-block" to="/">
-                Tyria UI
-              </TuiLink>
-              {header.breadcrumbs.map(({ label, link }, i) => (
-                <Fragment key={i}>
-                  &gt;
-                  {link ? <TuiLink to={link}>{label}</TuiLink> : <div>{label}</div>}
-                </Fragment>
-              ))}
-            </>
-          ) : (
-            'Tyria UI'
-          )}
-        </div>
-        <div className="flex gap-2">
+    <div className="flex px-6 bg-tui-dark max-w-dvw min-h-[72px]">
+      <div className="flex gap-2 md:gap-4 flex-1 items-center mx-auto w-full max-w-[1200px]">
+        <Button className="inline-flex md:hidden" onClick={() => onSideBarToggle()}>
+          <Menu />
+        </Button>
+        <TuiHeader />
+        <div className="flex gap-2 ml-auto">
           <Button as={NavLink} color="light" to="/settings">
             <Settings />
             <span className="hidden lg:inline-flex">Settings</span>
---
+++ src/components/common/TopBar/TuiHeader.tsx
@@ -0,0 +1,41 @@
+import { selectHeader } from '@/store/app.slice';
+import { Fragment, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { TuiIcon } from '../TuiIcon';
+import { TuiLink } from '../TuiLink';
+import { useBreakpoints } from '@/hooks/use-breakpoints';
+import * as styles from './TuiHeader.module.css';
+
+export function TuiHeader() {
+  const { mdAndUp } = useBreakpoints();
+  const header = useSelector(selectHeader);
+
+  const final_breadcrumb = useMemo(() => {
+    if (header.breadcrumbs.length === 0) return null;
+    return header.breadcrumbs[header.breadcrumbs.length - 1].label;
+  }, [header.breadcrumbs]);
+
+  if (mdAndUp) {
+    return (
+      <div className={styles.header}>
+        <TuiLink className="items-center gap-4" to="/">
+          <TuiIcon icon={header.image ?? '/favicon.png'} size={48} />
+          Tyria UI
+        </TuiLink>
+        {header.breadcrumbs.map(({ label, link }, i) => (
+          <Fragment key={i}>
+            &gt;
+            {link ? <TuiLink to={link}>{label}</TuiLink> : <div>{label}</div>}
+          </Fragment>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.header}>
+      <TuiIcon icon={header.image ?? '/favicon.png'} size={48} />
+      {final_breadcrumb ?? 'Tyria UI'}
+    </div>
+  );
+}
